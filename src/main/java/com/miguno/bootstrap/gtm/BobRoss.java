4767dfc9adab78a16a23ef63a6118882d470f385
Sunil Kumar <Sunil Kumar@192.168.0.99>
2016-02-21 14:09:38 +0530

    zzx

--- src/main/java/com/miguno/bootstrap/gtm/BobRoss.java
+++ src/main/java/com/miguno/bootstrap/gtm/BobRoss.java
@@ -12,6 +12,7 @@ import com.google.common.collect.ImmutableSet.Builder;
  * testing helloworld
  * tetsing 1
  * testing 2gg
+ * yyy
  * 
  * testint 3
  * object from painting elements and let our painter paint that painting object, for instance.)
