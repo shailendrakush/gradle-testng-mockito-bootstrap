3551ec765ea6db0933dcd2b4c67ee03c170c936d
Sunil Kumar <Sunil Kumar@192.168.0.99>
2016-02-21 12:47:40 +0530

    vv

--- src/main/java/com/miguno/bootstrap/gtm/BobRoss.java
+++ src/main/java/com/miguno/bootstrap/gtm/BobRoss.java
@@ -12,7 +12,7 @@ import com.google.common.collect.ImmutableSet.Builder;
  *ggggggggggggggggggggggggggggggggggggggggggggggg
  *
  *hhhhhhhhhhhhhhhhhhhjjjj
- * ggggggggggggggggggggggggsssggggggggggggggSS
+ * ggggggggggggggggggggggggsssggggggggggggggSSf
  * object from painting elements and let our painter paint that painting object, for instance.)
  * 
  * "And from all of us here I'd like to wish you happy painting and God bless, my friend."
