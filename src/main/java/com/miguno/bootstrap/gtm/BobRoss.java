17cc9dae83d4d56252dba92de8df513c7595f751
shailendrakush <shailendrakush@gmail.com>
2016-02-21 13:49:49 +0530

    Update BobRoss.java

--- src/main/java/com/miguno/bootstrap/gtm/BobRoss.java
+++ src/main/java/com/miguno/bootstrap/gtm/BobRoss.java
@@ -10,6 +10,7 @@ import com.google.common.collect.ImmutableSet.Builder;
  * 
  * testing master
  * testing helloworld
+ * tetsing 1
  * object from painting elements and let our painter paint that painting object, for instance.)
  * 
  * "And from all of us here I'd like to wish you happy painting and God bless, my friend."
