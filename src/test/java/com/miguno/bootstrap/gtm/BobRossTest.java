9edcbd8c877e6f12b368231d8edbd27339a22a18
Sunil Kumar <Sunil Kumar@192.168.0.99>
2016-02-21 14:49:41 +0530

    sssssssss

--- src/test/java/com/miguno/bootstrap/gtm/BobRossTest.java
+++ src/test/java/com/miguno/bootstrap/gtm/BobRossTest.java
@@ -17,6 +17,8 @@ import com.google.common.collect.ImmutableSet;
 /**
  * 
  * sss
+ * 
+ * kumar
  * This test class shows how to write unit tests with TestNG, Mockito and FEST-Assert 2.
  * 
  * Don't pay too much attention to the semantics of the actual tests.
